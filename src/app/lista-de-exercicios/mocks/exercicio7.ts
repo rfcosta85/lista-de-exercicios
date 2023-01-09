c1a5eaa62498c17a929818776fa642339f2099ce
Rodrigo Costa <rfcosta85@gmail.com>
2023-01-09 18:44:30 -0300

    Exercicio 07

--- src/app/lista-de-exercicios/mocks/exercicio7.ts
+++ src/app/lista-de-exercicios/mocks/exercicio7.ts
@@ -6,6 +6,10 @@ export class Exercicio7 implements ExerciciosBD {
     'Ler o nome de uma pessoa na forma “nome” seguido por “sobrenome” e escrever na forma “sobrenome” seguido por “nome”. Exemplo: ';
   public categoria = 1;
   public executar() {
+    console.clear();
     console.log('exercicio 07');
+    const primeiroNome = prompt('Digite o seu primeiro nome');
+    const sobrenome = prompt('Digite o seu sobrenome');
+    console.log(`${sobrenome} ${primeiroNome}`);
   }
 }
