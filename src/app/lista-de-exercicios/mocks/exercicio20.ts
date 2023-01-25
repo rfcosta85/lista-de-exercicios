ebcb8e7d7f7ddb26236efd059e5942f64b73727f
Rodrigo Costa <rfcosta85@gmail.com>
2023-01-25 12:44:02 -0300

    Exercicio 20

---
+++ src/app/lista-de-exercicios/mocks/exercicio20.ts
@@ -0,0 +1,57 @@
+import { ExerciciosBD } from './../models/exercicios.model';
+export class Exercicio20 implements ExerciciosBD {
+  public numero: number = 20;
+  public descricao: string = `Uma equipe deseja calcular o número mínimo de litros de combustível, 
+  que deve ser colocado no tanque de um carro de corrida, para que este possa completar um determinado número de voltas em um circuito, até o primeiro reabastecimento. Ler o comprimento da pista (em metros), o número total de voltas a serem percorridas, o número de reabastecimentos desejados e o consumo do carro (em Km/l). Em seguida, calcular e escrever o 
+  número mínimo de litros necessários para completar as voltas até o primeiro reabastecimento. Considerar que o número de voltas entre os reabastecimentos é o mesmo.`;
+  public categoria: number = 1;
+  public executar() {
+    console.clear();
+    console.log('Exercicio 20');
+    const comprimentoPista = Number(
+      prompt('Digite o comprimento da pista em Km')?.replace(',', '.').trim()
+    );
+    const numeroDeVoltasASerPercorrida = Number(
+      prompt('Digite o número total de voltas a ser percorrida')
+        ?.replace(',', '.')
+        .trim()
+    );
+    const numeroDeReabastecimentos = Number(
+      prompt('Digite a quantidade de reabastecimentos programada')
+        ?.replace(',', '.')
+        .trim()
+    );
+    const consumoDoCarro = Number(
+      prompt('Digite o consumo em Km/l do veículo')?.replace(',', '.').trim()
+    );
+    const consumoPorVolta = comprimentoPista / consumoDoCarro;
+    const litrosNecessariosParaConcluirACorrida =
+      consumoPorVolta * numeroDeVoltasASerPercorrida;
+    const primeiraParada =
+      litrosNecessariosParaConcluirACorrida / numeroDeReabastecimentos;
+
+    console.log(
+      `Comprimento da Pista: ${comprimentoPista.toLocaleString('br')} km`
+    );
+
+    console.log(
+      `Número de paradas Programadas: ${numeroDeReabastecimentos} paradas`
+    );
+
+    console.log(
+      `Consumo de combustível por volta: ${consumoPorVolta.toLocaleString(
+        'br',
+        { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+      )} litros`
+    );
+
+    console.log(
+      `Litros necessários para conclusão da corrida: ${litrosNecessariosParaConcluirACorrida.toLocaleString(
+        'br',
+        { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+      )} litros`
+    );
+
+    console.log(`Primeira Parada: ${primeiraParada.toFixed(0)}`);
+  }
+}
--- src/app/lista-de-exercicios/mocks/exercicios.mock.ts
+++ src/app/lista-de-exercicios/mocks/exercicios.mock.ts
@@ -1,3 +1,4 @@
+import { Exercicio20 } from './exercicio20';
 import { Exercicio19 } from './exercicio19';
 import { Exercicio18 } from './exercicio18';
 import { Exercicio17 } from './exercicio17';
@@ -29,14 +30,8 @@ export const ListaExerciciosMock: ExerciciosBD[] = [
   new Exercicio17(),
   new Exercicio18(),
   new Exercicio19(),
+  new Exercicio20(),
 
-  // {
-  //   numero: 20,
-  //   descricao:
-  //     'Uma equipe deseja calcular o número mínimo de litros de combustível, que deve ser colocado no tanque de um carro de corrida, para que este possa completar um determinado número de voltas em um circuito, até o primeiro reabastecimento. Ler o comprimento da pista (em metros), o número total de voltas a serem percorridas, o número de reabastecimentos desejados e o consumo do carro (em Km/l). Em seguida, calcular e escrever o número mínimo de litros necessários para completar as voltas até o primeiro reabastecimento. Considerar que o número de voltas entre os reabastecimentos é o mesmo.',
-  //   categoria: 1,
-  //   executar: ,
-  // },
   // {
   //   numero: 21,
   //   descricao: 'Ler um valor e escrever se é positivo, negativo ou zero.',
