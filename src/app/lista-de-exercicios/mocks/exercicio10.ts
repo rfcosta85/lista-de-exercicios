f565ccd32498a2680c738fd3da2ee44903bae849
Rodrigo Costa <rfcosta85@gmail.com>
2023-02-12 12:38:04 -0300

    Corrigindo o exercicio 10

--- src/app/lista-de-exercicios/mocks/exercicio10.ts
+++ src/app/lista-de-exercicios/mocks/exercicio10.ts
@@ -2,7 +2,7 @@ import { ExerciciosBD } from './../models/exercicios.model';
 export class Exercicio10 implements ExerciciosBD {
   public numero: number = 10;
   public descricao: string =
-    'Ler os valores S1, S2 e S3 correspondentes aos comprimentos dos três lados de um triângulo, calcular e escrever a sua área. A área do triângulo é dada por:';
+    'Ler os valores S1, S2 e S3 correspondentes aos comprimentos dos três lados de um triângulo, calcular e escrever a sua área. A área do triângulo é dada por: (AJUSTADO)';
   public categoria: number = 1;
   public executar() {
     console.clear();
@@ -16,7 +16,14 @@ export class Exercicio10 implements ExerciciosBD {
     const s3 = Number(
       prompt('Leia o terceiro lado do triângulo')?.replace(',', '.').trim()
     );
-    const area = (s1 + s2 + s3) / 2;
+    const ladosDoTriangulo = (s1 + s2 + s3) / 2;
+    const raizQuadrada = 1 / 2;
+    const area =
+      ladosDoTriangulo *
+      (ladosDoTriangulo - s1) *
+      (ladosDoTriangulo - s2) *
+      (ladosDoTriangulo - s3) *
+      raizQuadrada;
     console.log(
       `A área do triângulo será de: ${area.toLocaleString('br', {
         minimumFractionDigits: 3,
