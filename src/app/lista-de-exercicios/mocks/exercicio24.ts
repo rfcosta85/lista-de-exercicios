830345b725b61723868e5ceb6f34f00cc22fdae6
Rodrigo Costa <rfcosta85@gmail.com>
2023-02-26 12:59:35 -0300

    Ajustando exercicio 24

--- src/app/lista-de-exercicios/mocks/exercicio24.ts
+++ src/app/lista-de-exercicios/mocks/exercicio24.ts
@@ -2,7 +2,7 @@ import { ExerciciosBD } from './../models/exercicios.model';
 export class Exercicio24 implements ExerciciosBD {
   public numero: number = 24;
   public descricao: string =
-    'Ler três valores e escrever o menor deles. (AJUSTAR)';
+    'Ler três valores e escrever o menor deles. (AJUSTADO)';
   public categoria: number = 2;
   public executar() {
     console.clear();
@@ -26,6 +26,8 @@ export class Exercicio24 implements ExerciciosBD {
     if (valor1 < valor2 && valor1 < valor3) {
       console.log(`${valor1.toLocaleString('br')} foi o menor valor digitado`);
     } else if (valor2 > valor1 && valor2 > valor3) {
+      console.log(`${valor2.toLocaleString('br')} foi o menor valor digitado`);
+    } else if (valor1 > valor2 && valor3 > valor2) {
       console.log(`${valor2.toLocaleString('br')} foi o menor valor digitado`);
     } else {
       console.log(`${valor3.toLocaleString('br')} foi o menor valor digitado`);
